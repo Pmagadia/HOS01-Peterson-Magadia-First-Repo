c58f8cad127948e7ff3bc57fd3a22a2265fc220a
Pmagadia <magadiapeters@cityuniversity.edu>
2026-01-14 20:20:52 +0000

    Added comments to script

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,10 @@
-class HelloWorld:
+class HelloWorld: # A start of a simple class
 
-    def __init__(self):
-        self.message = "Hello, World"
+    def __init__(self): # Constructor that initializes the message
+        self.message = "Hello, World" # Setting the message attribute
         
-    def display(self):
-        print(self.message)
+    def display(self): # Method to display the message
+        print(self.message) # Print the message
 
-greeting = HelloWorld()
-greeting.display()
+greeting = HelloWorld() # Creating an instance of HelloWorld
+greeting.display() # To display the greeting message
